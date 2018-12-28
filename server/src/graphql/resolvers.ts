f476a798b684d6bd09d612cf640b837bff3b70c4
Alexander Kireev <farvistadw@gmail.com>
2018-12-28 19:25:34 +0700

    extract server to submodule

--- server/src/graphql/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
-import * as path from 'path';
-
-const resolversArray = fileLoader(path.join(__dirname, './resolvers'));
-
-export const resolvers = mergeResolvers(resolversArray);
